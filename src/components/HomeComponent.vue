370b6854e51d473acf3b1d70a5d38ed3746fcc44
hamzawan <ah.awan33@gmail.com>
2023-02-10 19:28:41 +0500

    Vue Tutorials

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Home component</h2>
+        <h2>{{email}}</h2>
+        <h2>{{name}}</h2>
+        <h2>{{getName("Rizwan")}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HomeComponent',
+    data(){
+        return {
+            email:"shahbazalivk@gmail.com",
+            name:"Shahbaz",
+            getName(a){
+                return a
+            }
+        }
+    }
+}
+</script>
